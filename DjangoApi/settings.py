a0525b1f19f943fba41b1953fd4d165849c90bcb
johnayuk <john1234ayuk@gmail.com>
2022-06-07 17:09:03 +0100

    cors headers

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -97,7 +97,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     # ' http://127.0.0.1:8000',
     "https://dzblog.herokuapp.com"
-    'https://dzblog.netlify.app'
+    # 'https://dzblog.netlify.app'
 ]
 # CORS_ORIGIN_ALLOW_ALL = True
 
