84bf2fa217eea3ddedd680f7334ec2fedeafd2e3
johnayuk <john1234ayuk@gmail.com>
2022-06-07 17:26:30 +0100

    cors headers

---
+++ api_basic/cors.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, response
+
+
+class CorsMiddleware(object):
+    def process_response(self, req, resp):
+        response["Access-Control-Allow-Origin"] = "*"
+        return response
